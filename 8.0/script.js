d384dc94871322fc3144270c6977b37ce4b7bed0
Liuhuaying <irenes2007@163.com>
2016-11-04 10:02:42 -0400

    finished assignment

--- 8.0/script.js
+++ 8.0/script.js
@@ -35,8 +35,8 @@ var axisY = d3.axisLeft()
 
 //Line generator
 var lineGenerator = d3.line()
-    .x(function(d){return scaleX(d.year)})
-    .y(function(d){return scaleY(d.value)})
+    .x(function(d){return scaleX(new Date(d.key))})
+    .y(function(d){return scaleY(d.averagePrice)})
     .curve(d3.curveCardinal);
 
 d3.queue()
@@ -46,6 +46,9 @@ d3.queue()
         //Mine the data to set the scales
         scaleX.domain( d3.extent(data,function(d){return d.travelDate}) );
         scaleColor.domain( airlines.values() );
+
+        //draw line
+        plot.append('path').attr('class','time-series');
 
         //Add buttons
         d3.select('.btn-group')
@@ -62,10 +65,12 @@ d3.queue()
             .on('click',function(d){
                 //Hint: how do we filter flights for particular airlines?
                 //data.filter(...)
-
+                var newarr=data.filter(function(el){return el.airline==d})
+                draw(newarr)
                 //How do we then update the dots?
             });
 
+        
         //Draw axis
         plot.append('g').attr('class','axis axis-x')
             .attr('transform','translate(0,'+h+')')
@@ -85,13 +90,58 @@ function draw(rows){
     flightsByTravelDate.forEach(function(day){
        day.averagePrice = d3.mean(day.values, function(d){return d.price});
     });
+    flightsByTravelDate.sort(function(a,b){return new Date(a.key)-new Date(b.key)})
 
-    console.log(flightsByTravelDate);
+    console.table(flightsByTravelDate);
 
     //Draw dots
+    var update=plot.selectAll('.node')
+        .data(rows, function(d){d.id})
 
+    var enter=update.enter()
+        .append('circle')
+        .attr('class','node')
+        .on('click',function(d){console.log(d.price)})
+        .on('mouseenter',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            tooltip.select('.title')
+                .html(d.airline + ', ' + d.travelDate.toLocaleDateString());
+            tooltip.select('.value')
+                .html('Price: $'+d.price);
+
+            tooltip.transition().style('opacity',1);
+
+            d3.select(this).style('stroke-width','1px');
+        })
+        .on('mousemove',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            var xy = d3.mouse( d3.select('.container').node() );
+            tooltip
+                .style('left',xy[0]+10+'px')
+                .style('top',xy[1]+10+'px');
+        })
+        .on('mouseleave',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            tooltip.transition().style('opacity',0);
+            d3.select(this).style('stroke-width','0px');
+        });
+    update.exit().remove();
+    update
+        .merge(enter)
+        .attr('fill',function(d){return scaleColor(d.airline)})
+        .attr('r',3)
+        .attr('cy',function(d){return scaleY(d.price)})
+        .attr('cx',function(d){return scaleX(d.travelDate)})
 
     //Draw <path>
+    plot.select('.time-series')       
+        .datum(flightsByTravelDate)
+        .transition()
+        .attr('d',function(el){return lineGenerator(flightsByTravelDate);})
+        .style('fill','none')
+        .style('stroke-width',2+'px')
+        .style('stroke',function(co){return scaleColor(co[0].values[0].airline)})
+       
 }
 
 function parse(d){
